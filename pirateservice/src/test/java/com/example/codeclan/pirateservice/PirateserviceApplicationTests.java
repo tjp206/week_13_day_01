77a56d7a97632f165cf89ff3d64c35dcc4391114
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-11-24 09:50:11 +0000

    finished debugging

--- pirateservice/src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
+++ pirateservice/src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
@@ -7,9 +7,12 @@ import com.example.codeclan.pirateservice.repositories.PirateRepository;
 import com.example.codeclan.pirateservice.repositories.RaidRepository;
 import com.example.codeclan.pirateservice.repositories.ShipRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PirateserviceApplicationTests {
 
